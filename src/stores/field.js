7648bbd2a21d0a6cf435fc19c41a03cefd042b90
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-19 14:39:54 -0600

    Add: Stores for beheabiour

---
+++ src/stores/field.js
@@ -0,0 +1,44 @@
+import { rnd } from '@/util/util'
+import { defineStore } from 'pinia'
+
+export const useFieldStore = defineStore('field', () => {
+  function gaussianPoint(mean, stdDev) {
+    let u = 0
+    let v = 0
+    while (u === 0) u = Math.random() // Evitar u = 0
+    while (v === 0) v = Math.random() // Evitar v = 0
+    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+    return mean + z * stdDev
+  }
+
+  const fillPoints = ($svg, $path, points) => {
+    const svgMatrix = $svg.getScreenCTM()
+    const length = $path.getTotalLength()
+    const step = length / points
+    const arrPoints = []
+
+    for (let i = 0; i < points; i++) {
+      const basePos = i * step
+      const normal = gaussianPoint(0, step / 2)
+      const position = Math.max(0, Math.min(length, basePos + normal))
+      const point = $path.getPointAtLength(position)
+
+      const screenX =
+        point.x * svgMatrix.a + point.y * svgMatrix.c + svgMatrix.e
+      const screenY =
+        point.x * svgMatrix.b + point.y * svgMatrix.d + svgMatrix.f
+
+      arrPoints.push({ x: screenX, y: screenY })
+    }
+
+    return arrPoints
+  }
+
+  function getPoints($svg, target, min, max) {
+    const $path = $svg.querySelector(target)
+    const numPoints = rnd(min, max)
+    return fillPoints($svg, $path, numPoints)
+  }
+
+  return { getPoints }
+})
