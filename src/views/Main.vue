6837ac260c89a3646107a699176a18653dfe529c
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-25 17:32:50 -0600

    Add: Flowers view

---
+++ src/views/Main.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <AIBackground />
+    <AIFirework />
+  </section>
+</template>
+
+<script setup>
+import AIFirework from '@/components/AIFirework.vue'
+import AIBackground from '@/components/background/AIBackground.vue'
+</script>
