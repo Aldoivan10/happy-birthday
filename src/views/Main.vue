184f3086b865ea111c5d27802b8eaadd5ad9f3af
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-26 14:38:20 -0600

    Refactor: Add events to load flowers on click button

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,11 @@
 <template>
-  <section>
-    <AIBackground />
-    <AIFirework />
-  </section>
+  <AIBackground ref="$bg" />
+  <AIFirework />
+  <AIIntro @load-flowers="$refs.$bg.init()" />
 </template>
 
 <script setup>
 import AIFirework from '@/components/AIFirework.vue'
+import AIIntro from '@/components/AIIntro.vue'
 import AIBackground from '@/components/background/AIBackground.vue'
 </script>
